72df84e8cc7b166bdd1a6016c5b857956bfd3a4f
Connor MacDonald <connor@hypack.com>
2021-12-06 21:21:30 -0500

    Revised day 6 part 2

--- 2021/day6/part2.py
+++ 2021/day6/part2.py
@@ -5,17 +5,8 @@ fishes = Counter((int(x) for x in stdin.readline().split(',')))
 for i in range(9):
   if i not in fishes:
     fishes[i] = 0
-
+    
 for i in range(256):
-  tmp = fishes[0]
-  fishes[0] = fishes[1]
-  fishes[1] = fishes[2]
-  fishes[2] = fishes[3]
-  fishes[3] = fishes[4]
-  fishes[4] = fishes[5]
-  fishes[5] = fishes[6]
-  fishes[6] = fishes[7] + tmp
-  fishes[7] = fishes[8]
-  fishes[8] = tmp
+  fishes[(i-2)%9] += fishes[i%9]
 
 print(sum(fishes.values()))
