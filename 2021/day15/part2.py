ef6bcbec4b15fb96cc95da0efafb77324de0434a
reddevild271 <reddevild271@users.noreply.github.com>
2021-12-16 03:22:33 -0500

    Much faster part2

--- 2021/day15/part2.py
+++ 2021/day15/part2.py
@@ -1,40 +1,37 @@
 from sys import stdin
 
-def in_bounds(xy, cave, mult=5):
+def in_bounds(xy, cave, mult):
   x,y = xy
-  return x in range(len(cave[0])*mult) and \
-         y in range(len(cave)*mult)
+  return x >= 0 and x < len(cave[0])*mult and \
+         y >= 0 and y < len(cave)*mult
 
 def risk_level(xy, cave):
   x,y = xy
   risk = cave[x%len(cave[0])][y%len(cave)]
   risk += x//len(cave[0]) + y//len(cave)
-  while risk > 9:
-    risk -= 9
-  return risk
+  return (risk - 1) % 9 + 1
 
 cave = [[int(risk) for risk in line[:-1]] for line in stdin]
 
 found = {}
-paths = [((0,0),0)]
 mult = 5
+paths = {(0,0):0}
 
 while len(paths) > 0:
-  path = paths[0]
-  paths = paths[1:]
-  paths.sort(key = lambda p: p[1])
-  if not in_bounds(path[0], cave):
-    continue
-  total_risk = path[1] + risk_level(path[0],cave)
-  if (len(cave[0])*mult-1,len(cave)*mult-1) in found:
-    if total_risk >= found[(len(cave[0])*mult-1,len(cave)*mult-1)]:
-      continue
-  if path[0] not in found or total_risk < found[path[0]]:
-    found[path[0]] = total_risk
-    paths.append(((path[0][0]+1,path[0][1]),total_risk))
-    paths.append(((path[0][0],path[0][1]+1),total_risk))
-    paths.append(((path[0][0]-1,path[0][1]),total_risk))
-    paths.append(((path[0][0],path[0][1]-1),total_risk))
+  path = min(paths, key=paths.get)
+  new_paths = [(path[0]-1,path[1]), \
+               (path[0],path[1]-1), \
+               (path[0]+1,path[1]), \
+               (path[0],path[1]+1)]
+  for new_path in new_paths:
+    if in_bounds(new_path, cave, mult) and new_path not in found:
+      risk = paths[path] + risk_level(new_path, cave)
+      if new_path not in paths:
+        paths[new_path] = risk
+      else:
+        paths[new_path] = min(risk, paths[new_path])
+  found[path] = paths[path]
+  del paths[path]
 
-print(found[(len(cave[0])*mult-1,len(cave)*mult-1)]-cave[0][0])
+print(found[(len(cave[0])*mult-1,len(cave)*mult-1)])
     
