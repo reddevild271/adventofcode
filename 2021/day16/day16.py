0b56f120cf74fc659d19f94aa560514f8b5806f4
reddevild271 <reddevild271@users.noreply.github.com>
2021-12-16 05:37:59 -0500

    Combined and refactored day 16

--- 2021/day16/day16.py
+++ 2021/day16/day16.py
@@ -1,5 +1,10 @@
 from sys import stdin
 
+def iterate(len_id, s, idx, i):
+  if len_id == '0':
+    return idx < int(s[7:22],2)+22
+  return i < int(s[7:18],2)
+
 def operate(type_id, items):
   if type_id == 0:
     return sum(items)
@@ -20,6 +25,7 @@ def operate(type_id, items):
     return 1 if items[0] == items[1] else 0
 
 def packet_parser(s):
+  version = int(s[:3],2)
   type_id = int(s[3:6],2)
   if type_id == 4:
     idx = 6
@@ -27,28 +33,17 @@ def packet_parser(s):
     while s[idx] == '1':
       idx += 5
       bits += s[idx+1:idx+5]
-    return (int(bits,2), idx+5)
+    return version, [int(bits,2)], idx+5
   len_id = s[6]
-  idx = 0
-  items = []
-  if len_id == '0':
-    length = int(s[7:22],2)
-    while idx < length:
-      result = packet_parser(s[idx+22:])
-      items.append(result[0])
-      idx += result[1]
-    return (operate(type_id, items), idx+22)
-  num_sub = int(s[7:18],2)
-  for i in range(num_sub):
-    result = packet_parser(s[idx+18:])
-    items.append(result[0])
-    idx += result[1]
-  return (operate(type_id, items), idx+18)
-      
-  
+  i = 0
+  result = [version, [], 22 if len_id == '0' else 18]
+  while iterate(len_id, s, result[2], i):
+    result = [x+y for x, y in zip(result, packet_parser(s[result[2]:]))]
+    i += 1
+  return result[0], [operate(type_id, result[1])], result[2]
+
 line = stdin.readline()[:-1]
 b_str = bin(int(line, 16))[2:].zfill(len(line)*4)
-
-print( packet_parser(b_str)[0] )
-
-
+result = packet_parser(b_str)
+print( 'Part 1: ' + str(result[0]) )
+print( 'Part 2: ' + str(result[1][0]) )
